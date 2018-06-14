9060d7c7be6e6c43bceacc4e37d79e5d69c7929a
Casper Voogt <casper.voogt@plethoradesign.com>
2018-06-14 16:39:37 -0400

    js

--- web/themes/custom/kyanite/js/kyanite.js
+++ web/themes/custom/kyanite/js/kyanite.js
@@ -1325,7 +1325,7 @@
     });
   }
   function eventSidebarDisplay(){
-    if($(".eventmap").is(":hidden")){//map is not visible
+    if($(".eventmap").attr("top") == "-10000px"){//map is not visible
       if (window.matchMedia('(max-width: 699px)').matches) {
         $(".layout-sidebar.mobile").show();
         $(".layout-sidebar.desktop").hide();
