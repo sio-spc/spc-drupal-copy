0d79225ae9f4d0142b7a661a5b7ab509f5117849
Casper Voogt <casper.voogt@plethoradesign.com>
2018-06-23 09:02:31 -0400

    updated geofield_map

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInitfef2afb14ff205ce5aaa2fe27a73a1c2
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitfef2afb14ff205ce5aaa2fe27a73a1c2', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
