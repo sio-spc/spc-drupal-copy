30ee97c2c282543f9f44ac2c01751df3c42285f0
Casper Voogt <casper.voogt@plethoradesign.com>
2018-07-17 18:02:09 -0400

    js

--- web/themes/custom/kyanite/js/kyanite.js
+++ web/themes/custom/kyanite/js/kyanite.js
@@ -1588,7 +1588,7 @@
   }
 
   function hideMain(){
-    consoleLog( "#main: " + $("#main").text() );
+    consoleLog( "#main:" + $("#main").text().trim() + ".(end)" );
     if($("#main").text().trim() == ""){
       $("#main").hide();
     }
