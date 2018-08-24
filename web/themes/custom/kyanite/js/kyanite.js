9c5b892064599e5bd77406a66c684ce6bd552c2a
Casper Voogt <casper.voogt@plethoradesign.com>
2018-08-24 13:51:05 -0400

    js

--- web/themes/custom/kyanite/js/kyanite.js
+++ web/themes/custom/kyanite/js/kyanite.js
@@ -1629,15 +1629,9 @@
     var months    = ['January','February','March','April','May','June','July','August','September','October','November','December'];
     var now       = new Date();
     var thisMonth = months[now.getMonth()]; // getMonth method returns the month of the date (0-January :: 11-December)
-    var output = $('.eventmonth');
+    $('.eventmonth').text(thisMonth);
     console.log(thisMonth);
 
-     if(output.textContent !== undefined) {
-        output.textContent = thisMonth;
-      }
-      else {
-        output.innerText = thisMonth;
-      }
 
 
     $(".eventsdateragepager .nav.prev").click(function(){
