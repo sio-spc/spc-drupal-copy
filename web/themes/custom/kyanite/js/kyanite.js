a56fdb1ee984c7330111573dd841dd1b007180e4
Casper Voogt <casper.voogt@plethoradesign.com>
2018-08-24 13:49:30 -0400

    js

--- web/themes/custom/kyanite/js/kyanite.js
+++ web/themes/custom/kyanite/js/kyanite.js
@@ -1625,7 +1625,21 @@
   }
 
   function eventDateRangePager(){
-    $(".path-events #block-pagetitle").after("<div class='eventsdateragepager'><a href='javascript:;' class='nav prev'>Prev</a><a href='javascript:;' class='nav next'>Next</a></div>");
+    $(".path-events #block-pagetitle").after("<div class='eventsdateragepager'><a href='javascript:;' class='nav prev'>Prev</a><a href='javascript:;' class='nav next'>Next</a></div><div class='eventmonth'></div>");
+    var months    = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    var now       = new Date();
+    var thisMonth = months[now.getMonth()]; // getMonth method returns the month of the date (0-January :: 11-December)
+    var output = $('.eventmonth');
+    console.log(thisMonth);
+
+     if(output.textContent !== undefined) {
+        output.textContent = thisMonth;
+      }
+      else {
+        output.innerText = thisMonth;
+      }
+
+
     $(".eventsdateragepager .nav.prev").click(function(){
 
       //min
