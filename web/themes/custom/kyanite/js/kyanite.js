3f2c12aae4df23d04fa1145b11279f517132305a
Casper Voogt <casper.voogt@plethoradesign.com>
2018-08-28 22:44:42 -0400

    js

--- web/themes/custom/kyanite/js/kyanite.js
+++ web/themes/custom/kyanite/js/kyanite.js
@@ -1649,7 +1649,7 @@
     var months = [
       'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
       ];
-    var eventspath = "/eventspath";
+    var eventspath = "/events";
   }
 
   function monthNumToName(monthnum) {
