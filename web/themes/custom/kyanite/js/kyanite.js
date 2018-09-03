1bf13f6fe6222b72384b61395f786e5b8f32de5d
Casper Voogt <casper.voogt@plethoradesign.com>
2018-09-03 18:31:09 -0400

    js

--- web/themes/custom/kyanite/js/kyanite.js
+++ web/themes/custom/kyanite/js/kyanite.js
@@ -1712,7 +1712,7 @@
         consoleLog("uid: " + uid);
         if((uid == "915") || (uid == "918") || (uid == "929")){
           $("#block-kyanite-local-tasks, div.contextual, #toolbar-administration").hide();
-          $("body").style("padding-top","0");
+          $("body").removeAttr("style");
         }
       }
     }
